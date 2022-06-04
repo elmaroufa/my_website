097b017788d50339b9e1b00b3f4a87ab93ab403d
elmaroufa <elmaroufa@gmail.com>
2022-06-04 13:28:18 +0100

    feature system comment add

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
+from re import search
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -8,6 +9,18 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class ComentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post','created_on', 'active')
+    list_filter = ('active','created_on')
+    search_fields = ['name', 'email', 'body']
+    active = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
 
 
 admin.site.register(Post,PostAdmin)
