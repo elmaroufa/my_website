d546af0929b6ce84d426fd0c717e1057b13fb2b6
elmaroufa <elmaroufa@gmail.com>
2022-06-06 11:08:29 +0100

    feature add system pagination

--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,32 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 from .forms import CommentForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+#class PostList(generic.ListView):
+#    queryset = Post.objects.filter(status=1).order_by('-created_on')
+#    template_name = 'index.html'
 
 #class PostDetail(generic.DetailView):
 #   model = Post
 #    template_name = 'post_detail.html'
+
+def post_list(request):
+    object_list = Post.objects.filter(status=1).order_by('-created_on')
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(object_list, 4)
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'page_obj' : page_obj})
+
 
 def post_detail(request, slug):
     template_name = 'post_detail.html'
